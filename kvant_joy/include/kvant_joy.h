41065ff0c2251a00a7b0a9f64036706c6a65f90a
kirillin <kaartemov@gmail.com>
2017-08-11 12:52:38 +0300

    add work version of joy

--- kvant_joy/include/kvant_joy.h
+++
@@ -1,4 +0,0 @@
-#include "ros/ros.h"
-
-ros::Subscriber sub;
-ros::Publisher pub;
--- kvant_joy/src/kvant_joy.cpp
+++ kvant_joy/src/kvant_joy.cpp
@@ -1,80 +1,100 @@
-#include "kvant_joy.h"
-#include "sensor_msgs/Joy.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
+#include "ros/ros.h"
+#include "sensor_msgs/Joy.h"
 #include "kvant_joy/CryptString.h"
 
-#include "std_msgs/String.h"
-#include <cmath>
-#include <iostream>
-#include <sstream>
+ros::Subscriber sub;
+ros::Publisher pub;
 
-const std::string KEY_FILE_NAME = "00024b1f_Alice.key";
-//
-// std::vector<char> XOR(std::vector<unsigned char> _data, char * _fn)
-// {
-//   //TODO: сделать проверку _data.size <= _fn.size
-//   //TODO: использовать файл ключа по кругу - для этого использовать static-переменную текущей позиции
-//
-//   using namespace std;
-//   vector<unsigned char> key;
-//   {
-//     ifstream _if(_fn, ios::binary);
-//     _if.seekg(0, ios::end);
-//     size_t fsize = _if.tellg();
-//     _if.seekg(0, ios::beg);
-//     key.resize(fsize);
-//
-//     _if.read((char*)&key[0], fsize);
-//   }
-//
-//   vector<char> ret(_data.size());
-//   for (size_t i = 0; i < _data.size(); ++i)
-//   {
-//     // Мякотка
-//     ret[i] = _data[i]^key[i];
-//   }
-//   return ret;
-// }
+char KEY_FILE_NAME[] = "/home/kirix/rosws/src/kvant/kvant_joy/key/00024b1f_Alice.key";
+static int SHIFT = 1;
 
-std::string float_to_str(float number) {
+std::vector<unsigned char> XOR(unsigned char *_data, char *_fn)
+{
+  size_t data_size = 25;
+  size_t fsize;
+  //TODO: сделать проверку _data.size <= _fn.size
+  //TODO: использовать файл ключа по кругу - для этого использовать static-переменную текущей позиции
+
+  using namespace std;
+  // reading key
+  vector<unsigned char> key;
+  ifstream _if(_fn, ios::binary);
+  if (_if.is_open()) {
+    _if.seekg(0, ios::end);
+    fsize = _if.tellg();
+    _if.seekg(0, ios::beg);
+    key.resize(fsize);
+    _if.read((char*) &key[0], fsize);
+  } else {
+    clog << "Opening key-file faild!" << endl;
+  }
+
+  // encrypting
+  vector<unsigned char> ret(data_size);
+  for(size_t i = 0; i < data_size; ++i) {
+    if(i > SHIFT * fsize) {
+      SHIFT++;
+    }
+    ret[i] = _data[i] ^ key[i + (SHIFT - 1) * fsize];
+  }
+  return ret;
+}
+
+std::string fts(float number) {
     std::ostringstream buff;
     buff << number;
     return buff.str();
 }
 
-void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
-{
-  // using namespace std;
-  // cout << "left_x\t" << msg->axes[0] << endl;
-  // cout << "left_y\t" << msg->axes[1] << endl;
-  // cout << "right_x\t" << msg->axes[2] << endl;
-  // cout << "right_y\t" << msg->axes[3] << endl;
-  float left_x = msg->axes[0];
-  float left_y = msg->axes[1];
-  float rot = msg->axes[0];
+void joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg) {
+  std::clog << "Joy event!\n";
 
-  // "-0.12 -3.45 -6.78"
-  char data[17];
-	snprintf(data, sizeof(data), "%f %f %f", round(left_x * 100) / 100,
-                                           round(left_y * 100) / 100,
-                                           round(rot * 100) / 100);
-	std::string data_str = data;
-	std::cout << "data: " << data_str << std::endl;
+  // geting values of axes pads
+  float left_x = joy_msg->axes[0];  // left-right move
+  float left_y = joy_msg->axes[1];  // forward--backward move
+  float right_x = joy_msg->axes[2]; // rotation move
+  float right_y = joy_msg->axes[3]; // dont useing
 
+  // creating string with commands from gamepad
+  // as "-0.23 0.01 -0.00"
+  int size_data = 25;
+	char data[size_data];
+	sprintf(data, "%.2f %.2f %.2f", left_x, left_y, right_x);
+  unsigned char *udata = reinterpret_cast<unsigned char*>(data);
 
-  kvant_joy::CryptString crypt_msg;
+  // unsigned char udata[size_data];
+	// for(int i = 0; i < size_data; i++) {
+	// 	udata[i] = static_cast<unsigned char>(data[i]);
+	// 	if (udata[i] == '\0') break;
+	// }
 
-  unsigned char udata[17];
+  std::clog << "raw data: " << udata << std::endl;
 
-  for(int i = 0; i < 17; i++) {
-    crypt_msg.crypt_string = static_cast<unsigned char>(data[i]);
+  // make message for ros
+  kvant_joy::CryptString crmsg;
+
+  // encrypt
+  std::vector<unsigned char> encrypt_data;
+  encrypt_data = XOR(udata, KEY_FILE_NAME);
+
+  // just test for -- decrypt
+  std::vector<unsigned char> decrypted_data;
+  decrypted_data = XOR(&encrypt_data[0], KEY_FILE_NAME);
+
+  std::cout << "encr. data: "<< &encrypt_data[0] << std::endl;
+  std::cout << "decr. data: "<< &decrypted_data[0] << std::endl;
+
+  // may be not need :)
+  for(int i = 0; i < encrypt_data.size(); i++) {
+    crmsg.crypt_string.push_back(encrypt_data[i]);
   }
+  pub.publish(crmsg);
 
-
-  // crypt_msg.crypt_string = XOR(data, KEY_FILE_NAME);
-
-  // std::cout << "crypted_data: " << crypt_msg.crypt_string;
-  pub.publish(crypt_msg);
+  std::clog << "Encrypted msg sent!\n";
 }
 
 int main(int argc, char **argv)
