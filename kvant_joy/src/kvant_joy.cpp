829fa666936cb7843b3fff0517ae3333536279f3
kirillin <kaartemov@gmail.com>
2017-08-14 17:05:09 +0300

    wrok first version

--- kvant_joy/src/kvant_joy.cpp
+++ kvant_joy/src/kvant_joy.cpp
@@ -10,7 +10,8 @@ ros::Subscriber sub;
 ros::Publisher pub;
 
 char KEY_FILE_NAME[] = "/home/kirix/rosws/src/kvant/kvant_joy/key/00024b1f_Alice.key";
-static int SHIFT = 1;
+static int SHIFT = 0;
+static int COUNT = 0;
 
 /*
   Encrypting/Decrypting Function
@@ -41,12 +42,34 @@ std::vector<unsigned char> XOR(unsigned char *_data, char *_fn)
 
   // encrypting
   vector<unsigned char> ret(data_size);
-  for(size_t i = 0; i < data_size; ++i) {
-    if(i > SHIFT * fsize) {
-      SHIFT++;
+
+  /*if data_size <= fsize*/
+  // std::clog << SHIFT << std::endl  ;
+  // int i = 0;
+  // int pos = (SHIFT - 1) * data_size;
+  // //    pos = 0; // !!!
+  // while (i < data_size) {
+  //   if(i + pos >= fsize) {
+  //     SHIFT = 1;
+  //     pos = (SHIFT - 1) * data_size;
+  //   }
+  //   ret[i] = _data[i] ^ key[i + pos];
+  //   i++;
+  // }
+
+  for (int i = 0; i < data_size; i++) {
+    if(SHIFT == fsize - 1) {
+      SHIFT = 0;
     }
-    ret[i] = _data[i] ^ key[i + (SHIFT - 1) * fsize];
+    ret[i] = _data[i] ^ key[i + SHIFT];
+
+    std::clog << (int) ret[i] << " xor ";
+    std::clog << (int) key[i + SHIFT] << " = ";
+    std::clog << (int) _data[i] << std::endl;
+
+   SHIFT++;
   }
+
   return ret;
 }
 
@@ -61,51 +84,76 @@ std::string fts(float number) {
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg) {
   std::clog << "Joy event!\n";
+  if(pub.getNumSubscribers() > 0) {
+    // geting values of axes pads
+    float left_x = joy_msg->axes[0];  // left-right move
+    float left_y = joy_msg->axes[1];  // forward--backward move
+    float right_x = joy_msg->axes[2]; // rotation move
+    float right_y = joy_msg->axes[3]; // dont useing
 
-  // geting values of axes pads
-  float left_x = joy_msg->axes[0];  // left-right move
-  float left_y = joy_msg->axes[1];  // forward--backward move
-  float right_x = joy_msg->axes[2]; // rotation move
-  float right_y = joy_msg->axes[3]; // dont useing
+    // creating string with commands from gamepad
+    // as "-0.23 0.01 -0.00"
+    int size_data = 25;
+    char data[size_data];
+    sprintf(data, "%.2f %.2f %.2f", left_x, left_y, right_x);
+    unsigned char *udata = reinterpret_cast<unsigned char*>(data);
 
-  // creating string with commands from gamepad
-  // as "-0.23 0.01 -0.00"
-  int size_data = 25;
-  char data[size_data];
-  sprintf(data, "%.2f %.2f %.2f", left_x, left_y, right_x);
-  unsigned char *udata = reinterpret_cast<unsigned char*>(data);
+    std::clog << "raw data: " << udata << std::endl;
 
-  std::clog << "raw data: " << udata << std::endl;
+    // make message for ros
+    kvant_joy::CryptString crmsg;
 
-  // make message for ros
-  kvant_joy::CryptString crmsg;
+    // encrypt
+    std::clog << SHIFT << std::endl;
+    std::vector<unsigned char> encrypt_data;
+    encrypt_data = XOR(udata, KEY_FILE_NAME);
 
-  // encrypt
-  std::vector<unsigned char> encrypt_data;
-  encrypt_data = XOR(udata, KEY_FILE_NAME);
+    std::clog << SHIFT << std::endl;
 
-  // just test for -- decrypt
-  std::vector<unsigned char> decrypted_data;
-  decrypted_data = XOR(&encrypt_data[0], KEY_FILE_NAME);
+    // just test for -- decrypt
+    // std::vector<unsigned char> decrypted_data;
+    // decrypted_data = XOR(&encrypt_data[0], KEY_FILE_NAME);
 
-  std::cout << "encr. data: "<< &encrypt_data[0] << std::endl;
-  std::cout << "decr. data: "<< &decrypted_data[0] << std::endl;
+    std::cout << "encr. data: "<< &encrypt_data[0] << std::endl;
+    // std::cout << "decr. data: "<< &decrypted_data[0] << std::endl;
 
-  // may be not need :)
-  for(int i = 0; i < encrypt_data.size(); i++) {
-    crmsg.crypt_string.push_back(encrypt_data[i]);
+    // may be not need :)
+    for(int i = 0; i < encrypt_data.size(); i++) {
+      crmsg.crypt_string.push_back((uint8_t) encrypt_data[i]);
+    }
+
+    // for(int i = 0; i < 25; i++) {
+    //   ROS_INFO("%d %.2X", (int) data[i], encrypt_data[i]);
+    // }
+
+    std::cout << "COUNT = " << COUNT << std::endl;
+    crmsg.id = COUNT;
+    COUNT++;
+    pub.publish(crmsg);
+
+    /* !!! if data_size <= fsize*/
+    // SHIFT++;
+
+    std::clog << "Encrypted msg sent!\n";
+  } else {
+    SHIFT = 0;
   }
-  pub.publish(crmsg);
-
-  std::clog << "Encrypted msg sent!\n";
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "listener_joy");
   ros::NodeHandle n;
-  sub = n.subscribe("joy", 100, joyCallback);
-  pub = n.advertise<kvant_joy::CryptString>("kvant_joy", 100);
-  ros::spin();
+  sub = n.subscribe("joy", 10, joyCallback);
+  pub = n.advertise<kvant_joy::CryptString>("kvant_joy", 10);
+  // ros::Duration(1).sleep();
+
+  ros::Rate R(20);
+  while(n.ok()) {
+    ros::spinOnce();
+    R.sleep();
+  }
+
+  // ros::spin();
   return 0;
 }
